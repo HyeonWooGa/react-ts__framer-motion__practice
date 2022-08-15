4f99c69fe9532ddcfaa31148d33cdb63e5c12e96
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-15 18:40:07 +0900

    Color Animating 예시, 정말 예쁘다

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, rgb(238, 0, 154), rgb(221, 0, 238));
 `;
 
 const Box = styled(motion.div)`
@@ -20,17 +21,20 @@ const Box = styled(motion.div)`
   font-weight: 600;
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-};
-
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [0.1, 1, 2]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 154), rgb(238, 178, 0))",
+    ]
+  );
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
