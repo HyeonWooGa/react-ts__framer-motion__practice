d5895346cb7bbca7204d7bd27f025a2fc6c14fec
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-15 18:00:40 +0900

    useTransform 사용 예시 (1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -28,13 +27,10 @@ const boxVariants = {
 
 function App() {
   const x = useMotionValue(0);
-  useEffect(() => {
-    x.onChange(() => console.log(x.get()));
-  }, [x]);
+  const scale = useTransform(x, [-800, 0, 800], [0.1, 1, 2]);
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>click me</button>
-      <Box style={{ x }} drag="x" dragSnapToOrigin />
+      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
