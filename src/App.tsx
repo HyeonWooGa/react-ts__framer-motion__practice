277cb86331fc3e9012f5859b8bb2966a8105a077
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-16 21:10:42 +0900

    Layout 심화

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   background: linear-gradient(135deg, rgb(0, 238, 154), rgb(238, 178, 0));
-  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -18,13 +17,14 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Circle = styled(motion.div)`
   height: 100px;
   width: 100px;
   background-color: #00a5ff;
-  border-radius: 50px;
 `;
 
 function App() {
@@ -32,13 +32,15 @@ function App() {
   const toggleClicked = () => setClicked((prev) => !prev);
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box
-        style={{
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start",
-        }}
-      >
-        <Circle layout />
+      <Box>
+        {!clicked ? (
+          <Circle style={{ borderRadius: 50 }} layoutId="circle" />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle style={{ borderRadius: 0, scale: 2 }} layoutId="circle" />
+        ) : null}
       </Box>
     </Wrapper>
   );
